117601966912fa2f07da24653f03aaed76b922d7
Anju Bose <anjuboseac@gmail.com>
2025-01-08 13:20:37 +0530

    new file added

---
+++ lab-docs/V1-Test.tf
@@ -0,0 +1,12 @@
+
+## Write your 1st terraform file
+   provider "aws" {
+  
+   region  = "us-east-1"
+   }
+ resource "aws_instance" "demo-server" {
+  ami           = "ami-01816d07b1128cd2d"
+  instance_type = "t2.micro"
+  key_name      = "terraform123"
+ }
+
